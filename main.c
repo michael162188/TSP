6af6f44fbe994f8770f283b5105f9f4659c72023
michael162188 <michaelpollard618@gmail.com>
2025-05-22 00:12:21 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define CITIES 10
+
+int nearestNeighbor();
+
+void genPerms();
+
+int main () {
+    int coordinates[20];
+    coordinates[0] = 0;
+    coordinates[1] = 0;
+
+    srand(time(0));
+
+    for (int i = 2; i < 20; i++) {
+        coordinates[i] = rand() % 201 - 100;
+    }
+    for (int i = 0; i < CITIES; i++) {
+        printf("City %d: (%d, %d)\n", i, coordinates[i], coordinates[i + 1]);
+    }
+}
